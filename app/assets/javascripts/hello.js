ec66e0cff955a7dc1e8d0c2a87f32b4d54d9175d
Dimytch <dimytch@a.toh.info>
2015-01-08 09:49:11 +0200

    requests deleting now works

--- app/assets/javascripts/hello.js
+++ app/assets/javascripts/hello.js
@@ -3,7 +3,4 @@ $(document).ready(function(){
     var rq_id = $(this).data('rq-id');
     $('#rq-raw-id-' + rq_id).toggleClass('hidden');
     });
-  $('#rqs-listing').on('ajax:success', '.delete-rq', function(e,data,status,xhr){
-    $('#rqs-listing').html(data);
-  });
 });
